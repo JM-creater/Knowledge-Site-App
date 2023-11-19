6002d3e043b541df7d9c13bc07dace7171476332
Rian Dave Brignas <rbrignas23@gmail.com>
2023-11-19 20:41:34 +0800

    Brignas/Alex - EditAdmin(Updated)

--- KnowledgeSiteApp.Models/Dto/UpdateUserDetailsDto.cs
+++ KnowledgeSiteApp.Models/Dto/UpdateUserDetailsDto.cs
@@ -13,6 +13,5 @@ namespace KnowledgeSiteApp.Models.Dto
         public string FirstName { get; set; }
         public string LastName { get; set; }
         public string Email { get; set; }
-        public string Password { get; set; }
     }
 }
